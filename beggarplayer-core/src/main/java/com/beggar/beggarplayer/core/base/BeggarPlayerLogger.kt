6d68524f83f95e8c955f23c6dd97a7fa07a0c5a9
BeggarLan <1348270542@qq.com>
2022-08-30 20:54:55 +0800

    埋点类

---
+++ beggarplayer-core/src/main/java/com/beggar/beggarplayer/core/base/BeggarPlayerLogger.kt
@@ -0,0 +1,24 @@
+package com.beggar.beggarplayer.core.base
+
+import android.util.Log
+
+/**
+ * author: BeggarLan
+ * created on: 2022/8/30 8:45 下午
+ * description: log
+ */
+class BeggarPlayerLogger {
+
+  companion object {
+    private const val TAG = "BeggarPlayer"
+
+    fun log(desc: String) {
+      Log.i(TAG, desc)
+    }
+
+    fun log(tag: String, desc: String) {
+      Log.i(TAG.plus("-").plus(tag), desc)
+    }
+  }
+
+}
