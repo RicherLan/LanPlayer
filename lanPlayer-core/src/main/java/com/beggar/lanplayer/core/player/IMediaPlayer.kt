f469ba42b6997922a5c502d9953f2911f42836ea
BeggarLan <1348270542@qq.com>
2022-08-30 10:40:59 +0800

    播放器接口

---
+++ lanPlayer-core/src/main/java/com/beggar/lanplayer/core/player/IMediaPlayer.kt
@@ -0,0 +1,64 @@
+package com.beggar.lanplayer.core.player
+
+/**
+ * author: BeggarLan
+ * created on: 2022/8/30 10:33 上午
+ * description: 播放器接口
+ */
+interface IMediaPlayer {
+
+  // ********************* 生命周期相关 *********************
+  /**
+   * 准备
+   */
+  fun prepare()
+
+  /**
+   * 异步准备
+   */
+  fun prepareAsync()
+
+  /**
+   * 开始
+   */
+  fun start()
+
+  /**
+   * 暂停
+   */
+  fun pause()
+
+  /**
+   * 停止
+   */
+  fun stop()
+
+  /**
+   * 重置
+   */
+  fun reset()
+
+  /**
+   * 释放
+   */
+  fun release()
+  // ********************* 生命周期相关 *********************
+
+  // ********************* 其他方法 *********************
+  /**
+   * 跳转到指定时间
+   */
+  fun seekTo(timeMs: Long)
+
+  /**
+   * 设置音量
+   */
+  fun setVolume(volume: Float)
+
+  /**
+   * 设置是否循环播放
+   */
+  fun setLoop(loop: Boolean)
+  // ********************* 其他方法 *********************
+
+}
