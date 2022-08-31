76cc6a8afa6acd17224dcb543a6ab5e3562e8aa7
BeggarLan <1348270542@qq.com>
2022-08-31 16:45:41 +0800

    datasource

---
+++ beggarplayer-core/src/main/java/com/beggar/beggarplayer/core/player/data/BeggarPlayerDataSource.kt
@@ -0,0 +1,14 @@
+package com.beggar.beggarplayer.core.player.data
+
+import android.content.Context
+import android.net.Uri
+
+/**
+ * author: BeggarLan
+ * created on: 2022/8/31 4:25 下午
+ * description: 播放器数据源
+ */
+data class BeggarPlayerDataSource(
+  val context: Context,
+  val uri: Uri
+)
