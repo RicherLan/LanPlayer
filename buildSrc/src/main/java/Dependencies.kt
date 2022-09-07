83dd0d6400f3a2eb960fd098171b35a860286625
BeggarLan <1348270542@qq.com>
2022-09-07 10:36:53 +0800

    buildSrc依赖管理

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,15 @@
+/**
+ * author: lanweihua
+ * created on: 2022/9/7 10:31 上午
+ * description: 依赖管理
+ */
+object Versions {
+  val retrofit = "2.3.0"
+  val rxjava = "2.1.9"
+}
+
+object Libs {
+  val retrofit = "com.squareup.retrofit2:retrofit:${Versions.retrofit}"
+  val retrofit_rxjava_adapter = "com.squareup.retrofit2:adapter-rxjava2:${Versions.retrofit}"
+  val rxjava = "io.reactivex.rxjava2:rxjava:${Versions.rxjava}"
+}
