bb2a14a3f74e3db0b7089d38697a50f171cd45b8
BeggarLan <1348270542@qq.com>
2022-09-09 11:52:27 +0800

    业务设置textureView

--- app/src/main/java/com/lan/player/MainActivity.kt
+++ app/src/main/java/com/lan/player/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.beggar.beggarplayer.core.BeggarPlayerController
 import com.beggar.beggarplayer.core.config.BeggarPlayerConfig
+import com.beggar.beggarplayer.core.view.BeggarPlayerTextureView
 
 /**
  * author: lanweihua
@@ -12,9 +13,23 @@ import com.beggar.beggarplayer.core.config.BeggarPlayerConfig
  */
 class MainActivity : AppCompatActivity() {
 
+  private lateinit var playerController: BeggarPlayerController
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
-    val playerController = BeggarPlayerController(BeggarPlayerConfig(null))
+
+    val textureView = findViewById<BeggarPlayerTextureView>(R.id.main_player_texture_view)
+    initPlayer(textureView)
   }
+
+  override fun onResume() {
+    super.onResume()
+  }
+
+  private fun initPlayer(textureView: BeggarPlayerTextureView) {
+    playerController = BeggarPlayerController(this, BeggarPlayerConfig(null))
+    playerController.setTextureView(textureView)
+  }
+
 }
